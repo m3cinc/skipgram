1193f6a1ca6443a68cb39852280739580eb5d01b
m3cinc <borowczak.marc@gmail.com>
2015-08-27 17:28:42 -0400

    Updated skip_ngram.R, added build_ngram.R and number_spell_words.R and US_currency_spell_words.R

---
+++ R/US_currency_spell_words.R
@@ -0,0 +1,16 @@
+#' US_currency_spell_words 
+#' @param x numeric 
+#' @return x as named num (name is the US_currency_spell_word) 
+#' @examples 
+#' US_currency_spell_words(101.45) returns a named num 101.45 named "one hundred one dollars dollars and 45 cents"
+#' US_currency_spell_words(-2001) returns a named num -2001, named "minus two thousand one dollars"
+US_currency_spell_words <- compiler::cmpfun ( function( x ) {
+        require ( httr, magrittr )
+        stopifnot( is.numeric( x ), is.finite( x ))         
+        httr::content( GET( paste0( "http://www.tools4noobs.com/online_tools/number-spell_words/?action=ajax_number_spell_words",
+                                    "&number=", x,
+                                    "&type=1",
+                                    "&locale=en_US" ) ), as = "text" ) -> result
+        result %<>% gsub('(<div class="well">)((\\S*\\s*)*)(<\\/div>)','\\2',.,ignore.case=TRUE,perl=TRUE,useBytes=TRUE)
+        setNames(x,result)
+}, options = list( optimize = 3) )
--- R/skip_ngram.R
+++ R/skip_ngram.R
@@ -2,16 +2,17 @@
 #' @param x character list, possibly compressed
 #' @param n integer, the gram type (1L for unigram, 2L for bigram, etc..) 
 #' @param skip integer, the number of tokens to skip over (0L for simple gram, 1L for one skip, i.e [token X token]), note skip is ignored, set to 0L, on unigrams!
-#' @param cflag integer, 1L if compressed string x is input
+#' @param cflag logical, TRUE indicates compressed string x is input
 #' @param filter is an optional character vector dictionary containing tokens to exclude, omit if no filter is supplied 
 #' @return skip_ngram as named integer vector x 
 #' @examples 
 #' skip_ngram(as.character(citation())) produces a unigram of citation in list x
 #' skip_ngram(string,3L) produces trigram of string in list x
 #' skip_ngram(string,2L,3L,1L,badwords) produces skip3-bigram in list x corresponding to [word X X X word] from a compressed list, excludes tokens in badwords list
-skip_ngram <- compiler::cmpfun (function( x,  n = 1L, skip = 0L, cflag = 0L, filter = NULL) {    
-        stopifnot( is.integer( n ), is.finite( n ), n > 0L, is.integer( skip ), is.finite( skip ), skip >-1L, is.integer( cflag ), is.finite( cflag ), cflag > -1L)
-        if ( cflag > 0L ) { x %>% memDecompress( "g", asChar = TRUE ) %>% strsplit( "\n" ) %>% unlist -> x }      # decompress
+skip_ngram <- compiler::cmpfun (function( x,  n = 1L, skip = 0L, cflag = FALSE, filter = NULL) {
+        require(stringi,magrittr)
+        stopifnot( is.integer( n ), is.finite( n ), n > 0L, is.integer( skip ), is.finite( skip ), skip >-1L, is.logical( cflag ) )
+        if ( cflag == TRUE ) { x %>% memDecompress( "g", asChar = TRUE ) %>% strsplit( "\n" ) %>% unlist -> x }      # decompress
         if ( n == 1L && skip > 0L ) { skip <- 0L } # ignore the skip
         stri_split_boundaries <- stringi::stri_split_boundaries 
         stri_join <- stringi::stri_join  
@@ -20,10 +21,12 @@ skip_ngram <- compiler::cmpfun (function( x,  n = 1L, skip = 0L, cflag = 0L, fil
         if (length ( filter > 0L ) ) { tokens <- tokens[ !tokens %in% filter ] }  # filter designated vocabulary
         # if we didn't detect any words or number of tokens is less than n return empty vector 
         if ( all( is.na( tokens )) || length( tokens ) < n + skip ) { character(0) } else { 
-        if ( skip == 0) { sapply(1:( length( tokens ) - n - 1L ),       function(i) stri_join( tokens[i:(i + n - 1L)], collapse = " "))} else {
-                          sapply(1:( length( tokens ) - n - skip -1L ), function(i) stri_join( c( tokens[i], tokens[(i + skip + 1L):(i + skip + n - 1L)]), collapse = " ")) } }
+        if ( skip == 0L) { sapply(1:( length( tokens ) - n - 1L ),       function(i) stri_join( tokens[i:(i + n - 1L)], collapse = " "))} else {
+                           sapply(1:( length( tokens ) - n - skip -1L ), function(i) stri_join( c( tokens[i], tokens[(i + skip + 1L):(i + skip + n - 1L)]), collapse = " ")) } }
 } , options = list( optimize = 3) )
 
 # string <- c( "This scope of this report is to perform exploratory data analysis on the sample data ",
 # "set. The specific data set contains English, German, Russian and Finnish language data sets for",
-# " exploration. This milestone report focuses on exploring English language datasets within the specific ")
+# " exploration. This milestone report focuses on exploring English language datasets within the specific,
+# "data set. This data set contains en_US.news.txt, en_US.blogs.txt, and en_US.twitter.txt files." )
+
